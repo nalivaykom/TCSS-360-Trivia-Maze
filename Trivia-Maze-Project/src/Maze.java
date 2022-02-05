9105672fa095b41cf239096003af796a071dbdd4
nalivaykom <nalim2@uw.edu>
2022-02-05 14:10:38 -0800

    updated maze and room

--- Trivia-Maze-Project/src/Maze.java
+++ Trivia-Maze-Project/src/Maze.java
@@ -1,9 +1,14 @@
 
+
 public class Maze {
 
+	private Maze() {
+		Room MazeRooms[7][7];
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello world");
+        
 	}
 
 }
--- Trivia-Maze-Project/src/Room.java
+++ Trivia-Maze-Project/src/Room.java
@@ -1,6 +1,11 @@
+import java.util.*;
 
-public class Room {
+public class Room extends Maze {
 
+	private Room() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
