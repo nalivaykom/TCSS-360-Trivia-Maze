fec2fb5a0921a34c3a9246a33e312e82307a12fd
ArturNalivayko <arturnalivayko@gmail.com>
2022-02-06 21:26:46 -0800

    added coordinates (rows and columns for the Player class, including the getters and setters.

--- Trivia-Maze-Project/src/Player.java
+++ Trivia-Maze-Project/src/Player.java
@@ -1,12 +1,28 @@
-import java.util.*;
-
 public class Player {
 	
 	String name;
-	String color;
+	int row;
+	int column;
 	
-	Player(String n) {
-		name= n;
-
+	Player(String theName, int theRow, int theColumn) {
+		name = theName;
+		this.row = theRow;
+		this.column = theColumn;
+	}
+	
+	public void setName(String theName) {
+		name = theName;
+	}
+	
+	public void setRow(int theRow) {
+		row = theRow;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public int getColumn() {
+		return this.column;
 	}
 }
