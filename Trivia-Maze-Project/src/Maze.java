d239ec9c69fada1726dea78659b56a45351bc4b0
nalivaykom <nalim2@uw.edu>
2022-02-06 22:57:30 -0800

    made MazeRooms a class variable

--- Trivia-Maze-Project/src/Maze.java
+++ Trivia-Maze-Project/src/Maze.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Maze {
 
+	private Room[][] MazeRooms;
+	
 	Maze() {
-		Room[][] MazeRooms = new Room[7][7];
+		MazeRooms = new Room[7][7];
 		
 		for (int i = 1; i < 6; i++) {
 			for (int j = 1; j < 6; j++) {
