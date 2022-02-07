9789f755f6738ba796d7fb3d117be0bce0547bf4
ArturNalivayko <arturnalivayko@gmail.com>
2022-02-06 21:05:46 -0800

    created Door class with getters and setters

--- Trivia-Maze-Project/src/Door.java
+++ Trivia-Maze-Project/src/Door.java
@@ -1,5 +1,27 @@
-import java.util.*;
-
 public class Door {
+	
+	private boolean permLocked;
+	private boolean locked;
+
+	public Door() {
+		permLocked = false;
+		locked = true;
+	}
+	
+	public void setPermLocked(boolean thePermLocked) {
+		permLocked = thePermLocked;
+	}
+	
+	public void setLocked(boolean theLocked) {
+		locked = theLocked;
+	}
+	
+	public boolean getLockStat() {
+		return locked;
+	}
+	
+	public boolean getPermLockStat() {
+		return permLocked;
+	}
 	
 }
