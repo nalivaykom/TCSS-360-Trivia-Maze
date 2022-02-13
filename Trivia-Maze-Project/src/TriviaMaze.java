6e21b0d047a153568a593554755aded4e1fb7fb9
nalivaykom <nalim2@uw.edu>
2022-02-12 16:47:46 -0800

    got rid of javafx 32 bit and replaced with 64 bit and added buttons

--- Trivia-Maze-Project/src/TriviaMaze.java
+++ Trivia-Maze-Project/src/TriviaMaze.java
@@ -1,11 +1,12 @@
-
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TriviaMaze extends Application{
@@ -14,13 +15,7 @@ public class TriviaMaze extends Application{
 	Button leftButton;
 	Button rightButton;
 	Button downButton;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        
-		launch(args);
-	}
-	
+		
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
@@ -28,22 +23,49 @@ public class TriviaMaze extends Application{
 		
 		upButton = new Button();
 		upButton.setText("Up");
+		upButton.setMinSize(50, 25);
+		upButton.setMaxSize(50, 25);
+		
 		leftButton = new Button();
 		leftButton.setText("Left");
+		leftButton.setMinSize(50, 25);
+		leftButton.setMaxSize(50, 25);
+		
 		rightButton = new Button();
 		rightButton.setText("Right");
+		rightButton.setMinSize(50, 25);
+		rightButton.setMaxSize(50, 25);
+		
 		downButton = new Button();
 		downButton.setText("Down");
+		downButton.setMinSize(50, 25);
+		downButton.setMaxSize(50, 25);
+		
+		VBox upDownButtonsBox = new VBox(25);
+		HBox allButtonsBox = new HBox();
+		upDownButtonsBox.getChildren().addAll(upButton, downButton);
+		allButtonsBox.getChildren().addAll(leftButton, upDownButtonsBox, rightButton);
+		allButtonsBox.setMinSize(75, 150);
+		allButtonsBox.setMaxSize(75, 150);
+		allButtonsBox.setAlignment(Pos.CENTER);
 		
 		StackPane layout = new StackPane();
-		layout.getChildren().add(upButton);
-		layout.getChildren().add(leftButton);
-		layout.getChildren().add(rightButton);
-		layout.getChildren().add(downButton);
+		layout.setMinSize(300, 500);
+		layout.setMaxSize(300, 500);
+		layout.getChildren().add(allButtonsBox);
+//		layout.getChildren().add(upButton);
+//		layout.getChildren().add(leftButton);
+//		layout.getChildren().add(rightButton);
+//		layout.getChildren().add(downButton);
 		
 		Scene scene = new Scene(layout, 300, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 
 }
