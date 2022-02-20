7551a89166dfe45d02cfed91fe3e65418c0d8f01
nalivaykom <nalim2@uw.edu>
2022-02-20 15:39:40 -0800

    adding question_answer changes

--- Trivia-Maze-Project/src/Question_Answer.java
+++ Trivia-Maze-Project/src/Question_Answer.java
@@ -1,4 +1,20 @@
 
 public class Question_Answer {
 
+	private String question = "what is 3 + 3?";
+	private String questionType = "Short Answer";
+	private String answer = "6";
+	
+	String getQuestion() {
+		return this.question;
+	}
+	
+	String getQuestionType() {
+		return this.questionType;
+	}
+	
+	String getAnswer() {
+		return this.answer;
+	}
+	
 }
